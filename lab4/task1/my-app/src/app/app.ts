07d66aefa3be99998afdfeaa49e9c6c716db7451
Aruzhanrash <ar_rashit@kbtu.kz>
2026-02-19 22:50:25 +0500

    .

---
+++ lab4/task1/my-app/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+  template: `
+    Hello Universe
+  `,
+
+  styles: [`
+    :host {
+      color: #a144eb;
+      font-size: 40px;
+    }
+  `]
+})
+export class App { }
